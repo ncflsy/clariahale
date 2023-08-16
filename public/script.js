de012e38d3753cfb2aa664e16648015014c93681
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2023-08-16 12:44:14 +0700

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn-menu").addEventListener("click", function() {
+    const menu = document.getElementById("menu");
+    menu.scrollIntoView({ behavior: "smooth" });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      pink: '#F5E3E5',
+      orange: '#F38F58',
+      redVelvet: '#6D092C',
+      brown: '#433939',
+      black: '#000000',
+      white: '#FFFFFF',
+      lightGrey: '#D7D7D7',
+    },
+    fontFamily: {
+      outfit: ["Outfit", "sans-serif"],
+     },
+  },
+  plugins: [],
+}
+
