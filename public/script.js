ace18a0182448fc77d83c5e00b6038ad5164f7b0
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2023-08-26 20:27:34 +0700

    menambahkan slider, footer, & navbar

--- public/script.js
+++ public/script.js
@@ -2,3 +2,24 @@ document.getElementById("btn-menu").addEventListener("click", function() {
     const menu = document.getElementById("menu");
     menu.scrollIntoView({ behavior: "smooth" });
 });
+
+const navLine = document.querySelector("#nav-button");
+var nav = document.querySelector('nav');
+
+navLine.addEventListener('click', function(){
+    navLine.classList.toggle('nav-active');
+    nav.classList.toggle('hidden');
+});
+
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+    var scrollTop = window.scrollY;
+
+    if (scrollTop > 0) {
+        header.classList.add('navbar-fixed'); 
+        
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+});
+
